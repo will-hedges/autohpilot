234294e7d31f969ef503e70f216c587a69078049
Will Hedges <wshedges@outlook.com>
2023-02-01 08:29:30 -0700

    initial commit from boilerplate * based off honey-rae-repairs * login working * marked out navbar links * basic titles changed * code formatted for consistency

---
+++ src/components/views/ApplicationViews.js
@@ -0,0 +1,8 @@
+export const ApplicationViews = () => {
+  return (
+    <>
+      <h1 className="title--main">Auto(HPI)lot</h1>
+      <div>Decreasing charting fatigue, one note at a time.</div>
+    </>
+  );
+};
