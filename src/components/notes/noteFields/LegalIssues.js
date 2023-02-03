33cb0364f41dcb7d771ce5b0b92b6c018b9aa740
Will Hedges <wshedges@outlook.com>
2023-02-03 00:36:57 -0700

    changed textarea to radio buttons + notes * identical to other components now * closes #18

--- src/components/notes/noteFields/LegalIssues.js
+++ src/components/notes/noteFields/LegalIssues.js
@@ -1,19 +1,57 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const LegalIssues = () => {
-  const [legalIssues, setLegalIssues] = useState("");
+  const [legalIssues, setLegalIssues] = useState();
+  const [legalIssuesNotes, setLegalIssuesNotes] = useState("");
+
+  const handleLegalIssuesBool = (evt) => {
+    let boolState = evt.target.value === "true" ? true : false;
+    setLegalIssues(boolState);
+  };
+
+  useEffect(() => {
+    if (!legalIssues) {
+      setLegalIssuesNotes("");
+    }
+  }, [legalIssues]);
 
   return (
     <div className="form-group">
-      <label htmlFor="legalIssues">Legal Issues:</label>
-      <textarea
-        type="text"
+      <label htmlFor="legalIssues">History of legal issues:</label>
+
+      <input
+        type="radio"
+        value={true}
+        id="legalIssuesYes"
+        onChange={handleLegalIssuesBool}
         name="legalIssues"
-        value={legalIssues}
-        onChange={(evt) => {
-          setLegalIssues(evt.target.value);
-        }}
       />
+      <label htmlFor="legalIssuesYes">Yes</label>
+
+      <input
+        type="radio"
+        value={false}
+        id="legalIssuesNo"
+        onChange={handleLegalIssuesBool}
+        name="legalIssues"
+      />
+      <label htmlFor="legalIssuesNo">No</label>
+
+      {legalIssues ? (
+        <div className="form-group notes-field">
+          <label htmlFor="legalIssuesNotes">Notes:</label>
+          <input
+            type="text"
+            value={legalIssuesNotes}
+            id="legalIssuesNotes"
+            onChange={(evt) => {
+              setLegalIssuesNotes(evt.target.value);
+            }}
+          />
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
