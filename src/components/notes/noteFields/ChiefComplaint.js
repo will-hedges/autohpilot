b363632fba137d8946b6d4d68e08f941968be9db
Will Hedges <wshedges@outlook.com>
2023-02-03 08:18:30 -0700

    added ChiefComplaint field, hooks, labels

---
+++ src/components/notes/noteFields/ChiefComplaint.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+export const ChiefComplaint = () => {
+  const [chiefComplaint, setChiefComplaint] = useState("");
+
+  return (
+    <div className="form-group">
+      <label htmlFor="chiefComplaint">Chief Complaint:</label>
+      <input
+        type="text"
+        name="chiefComplaint"
+        value={chiefComplaint}
+        onChange={(evt) => {
+          setChiefComplaint(evt.target.value);
+        }}
+      />
+    </div>
+  );
+};
