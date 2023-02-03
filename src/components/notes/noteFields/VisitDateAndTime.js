34744a026e9d71da520e27caea131dbba9287681
Will Hedges <wshedges@outlook.com>
2023-02-03 08:31:02 -0700

    added visitTime field, combined VisitDate and VisitTime components into one * renamed VisitDate.js to VisitDateAndTime.js * used 'stock' time picker * sets state to time string, should be able to use for sorting * progresses #7, closes #28

---
+++ src/components/notes/noteFields/VisitDateAndTime.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export const VisitDateAndTime = () => {
+  const [visitDate, setVisitDate] = useState();
+  const [visitTime, setVisitTime] = useState();
+
+  return (
+    <div className="form-group">
+      <label htmlFor="visitDate">Visit Date:</label>
+      <input
+        type="date"
+        name="visitDate"
+        value={visitDate}
+        onChange={(evt) => {
+          setVisitDate(evt.target.value);
+        }}
+      />
+
+      <label htmlFor="visitTime">Visit Time:</label>
+      <input
+        type="time"
+        name="visitTime"
+        value={visitTime}
+        onChange={(evt) => {
+          setVisitTime(evt.target.value);
+        }}
+      />
+    </div>
+  );
+};
