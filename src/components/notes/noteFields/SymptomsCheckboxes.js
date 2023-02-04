717599d2fd44802ff59a5693b9e82f0afe7bfa85
Will Hedges <wshedges@outlook.com>
2023-02-04 12:14:26 -0700

    added logic to get all checked symptoms in an array

--- src/components/notes/noteFields/SymptomsCheckboxes.js
+++ src/components/notes/noteFields/SymptomsCheckboxes.js
@@ -5,6 +5,20 @@ const API = "http://localhost:8088";
 export const SymptomsCheckboxes = () => {
   const [symptoms, setSymptoms] = useState([]);
 
+  let checkedSymptoms = [];
+
+  const handleCheckboxChange = (evt) => {
+    const symptom = evt.target;
+    const symptomId = evt.target.value;
+    if (symptom.checked) {
+      // add to checkedSymptoms
+      checkedSymptoms.push(symptomId);
+    } else {
+      // remove from checkedSymptoms
+      checkedSymptoms = checkedSymptoms.filter((val) => val !== symptomId);
+    }
+  };
+
   useEffect(() => {
     fetch(`${API}/symptoms`)
       .then((res) => res.json())
@@ -21,12 +35,15 @@ export const SymptomsCheckboxes = () => {
           <div>
             <input
               type="checkbox"
-              class="symptom-checkbox"
+              className="symptom-checkbox"
               key={symptom.id}
               value={symptom.id}
               name={symptom.name}
+              onChange={handleCheckboxChange}
             />
-            <label htmlFor={symptom.name}>{symptom.name}</label>
+            <label htmlFor={symptom.name} key={`symptom--${symptom.id}`}>
+              {symptom.name}
+            </label>
           </div>
         );
       })}
