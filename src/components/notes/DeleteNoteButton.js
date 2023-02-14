e5080041edb0faace055f42d7b340583cd616779
Will Hedges <wshedges@outlook.com>
2023-02-14 10:59:48 -0600

    delete button now deletes noteSymptoms, noteSubstances * fixes a bug where old symptoms, substances would show up on the wrong note * closes #73

--- src/components/notes/DeleteNoteButton.js
+++ src/components/notes/DeleteNoteButton.js
@@ -3,7 +3,26 @@ const API = "http://localhost:8088";
 export const DeleteNoteButton = ({ notes, setNotes, chosenNote }) => {
   const handleDeleteButtonClick = () => {
     if (window.confirm("Are you sure you want to delete this note?")) {
-      fetch(`${API}/notes/${chosenNote.id}`, { method: "DELETE" }).then(() => {
+      const promises = [];
+      // add a promise to delete the note itself
+      promises.push(
+        fetch(`${API}/notes/${chosenNote.id}`, { method: "DELETE" })
+      );
+      // add promises for deleting note symptoms
+      promises.push(
+        fetch(`${API}/noteSymptoms?noteId=${chosenNote.id}`, {
+          method: "DELETE",
+        })
+      );
+      // add promises for deleting the note substances
+      promises.push(
+        fetch(`${API}/noteSubstances?noteId=${chosenNote.id}`, {
+          method: "DELETE",
+        })
+      );
+
+      Promise.all(promises).then(() => {
+        // filter out the deleted note from your state
         const remainingNotes = notes.filter(
           (note) => note.id !== chosenNote.id
         );
